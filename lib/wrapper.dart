fbb9b4ef6189b9b8af11b84acb030a8b7ff73a2d
ryokucha <128300075+DaniilShayd@users.noreply.github.com>
2024-11-28 12:22:02 +0300

    .

--- lib/wrapper.dart
+++ lib/wrapper.dart
@@ -1,12 +1,12 @@
 import 'package:flutter/material.dart';
-import 'package:hackaton_333/features/feed_loader/presentation/feed_loader_screen.dart';
+import 'package:hackaton_333/home_page.dart';
 
 class Wrapper extends StatelessWidget{
   const Wrapper({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const FeedLoaderScreen();
+    return const MyHomePage();
     // return MultiBlocProvider(providers: [
     //   // BlocProvider<SendAndLoadingFileBloc>( оставил для примера
     //   //     create: (context) => SendAndLoadingFileBloc()),
