fb87766f18d84e4e17304cdae02a31839b5322da
ryokucha <128300075+DaniilShayd@users.noreply.github.com>
2024-11-30 01:04:37 +0300

    init hive in main

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:hackaton_333/core/domain/global/di/injectable_setup.dart';
+import 'package:hive/hive.dart';
+import 'package:path_provider/path_provider.dart' as path_provider;
+
 import 'package:hackaton_333/wrapper.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-
   await configureDependencies();
+  final appDocumentDir = await path_provider.getApplicationDocumentsDirectory();
+  Hive.init(appDocumentDir.path);
   runApp(const Wrapper());
 }
